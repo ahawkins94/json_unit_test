c599aad08c51b36ca2d229af00a6843b58572830
ahawkins94 <adamhawkins94@hotmail.co.uk>
2017-10-18 23:35:41 +0100

    this is an infuriating error....

--- spec/json_test_spec.rb
+++ spec/json_test_spec.rb
@@ -7,11 +7,11 @@ describe 'JSON example tests' do
   end
 
   it 'should have the key colour for each data object' do
-    expect(@json['colors'][0].first).to be ["color", "black"]
+    expect(@json['colors'][0].keys).to include ["color"]
   end
 
   it "the colour red should have the hex value of '#FF0' " do
-
+    # expect(@json['colors'][2].first).to be ["color", "black"]
   end
 
   it "the colour green should have the RGBA value of '0,255,0,1' " do
